24f67fa8e4b5d1dfd14c8bc890e47ea60200a75a
ngaanhng <hoangwoo25@gmail.com>
2026-08-15 12:29:20 +0000

    ver30

--- script.js
+++ script.js
@@ -254,8 +254,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- AUDIO BACKGROUND MUSIC ---
     const audioStreamUrls = [
         "assets/bg_music_custom.mp3",
-        "https://docs.google.com/uc?export=download&id=1gjKr5oubskjxK5ysOicZ_wViTbrPmkp6",
-        "https://drive.google.com/uc?export=download&id=1gjKr5oubskjxK5ysOicZ_wViTbrPmkp6"
+        "https://docs.google.com/uc?export=download&id=1scP3S3b6JqunkoWR6qPFmQ1jrQK-MuI-",
+        "https://drive.google.com/uc?export=download&id=1scP3S3b6JqunkoWR6qPFmQ1jrQK-MuI-"
     ];
     let currentAudioUrlIndex = 0;
 
